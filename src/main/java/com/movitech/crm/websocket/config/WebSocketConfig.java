7f25e2110bec0bc7e810afd8ae10292d7af2cc7b
Silence.Zheng <Silence.Zheng@movit-tech.com>
2018-11-01 16:05:10 +0800

    setAllowedOrigins("*");

--- src/main/java/com/movitech/crm/websocket/config/WebSocketConfig.java
+++ src/main/java/com/movitech/crm/websocket/config/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(myHandler(), "/myHandler").addInterceptors(new WebSocketInterceptor());
+        webSocketHandlerRegistry.addHandler(myHandler(), "/myHandler").addInterceptors(new WebSocketInterceptor()).setAllowedOrigins("*");
     }
 
     /**
